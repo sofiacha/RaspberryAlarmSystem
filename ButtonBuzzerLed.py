4a9009a1dbfff77d22ff17c0525bd5f97e939a72
Sofia Chatzopoulou <sofaki12@msn.com>
2017-05-11 18:32:38 +0300

    Update and rename les4.py to ButtonBuzzerLed.py

--- ButtonBuzzerLed.py
+++ ButtonBuzzerLed.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#  les4.py
-#  
-#  Copyright 2017  <pi@raspberrypi>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
 
 import time
 import os
@@ -29,13 +8,15 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 #GPIO.cleanup()
 GPIO.setwarnings(False)
-	
+#red led	
 GPIO.setup(17,GPIO.OUT)
+#blue led
 GPIO.setup(27,GPIO.OUT)
+#buzzer
 GPIO.setup(22,GPIO.OUT)
-
+#button
 GPIO.setup(10,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+#asks for a choice between red and blue led
 led = raw_input("Which led? \n 1) Red \n 2) Blue \n")
 
 print "Push the button as many times as you wish to indicate times that a led can blink."
@@ -46,7 +27,7 @@ time.sleep(1)
 
 print "Wait for you to press the button!"
 print "You have 20 seconds"
-
+#the led of your choise will be blinked depending on how many times you pressed the button
 while True:	
 	
 	if (GPIO.input(10) == False):
@@ -54,7 +35,7 @@ while True:
 		GPIO.output(22,GPIO.HIGH)
 		time.sleep(.1)
 		GPIO.output(22,GPIO.LOW)
-		
+		#everytime you press the button the buzzer will inform you
 		if times==1:
 			print "Button pressed %s time" %(times) 
 		else:
@@ -69,6 +50,7 @@ while True:
 if (led.lower() == "red") :
 	print "Red started blinking"
 	for i in range(1,times+1):
+		#red led and buzzer turn on as many times as it is indicated from the button
 		GPIO.output(22,GPIO.HIGH)		
 		GPIO.output(17,GPIO.HIGH)
 		time.sleep(.1)
@@ -80,6 +62,7 @@ if (led.lower() == "red") :
 elif (led.lower() == "blue"):
 	print "Blue started blinking"
 	for i in range(1,times+1):
+		#blue led and buzzer turn on as many times as it is indicated from the button
 		GPIO.output(22,GPIO.HIGH)		
 		GPIO.output(27,GPIO.HIGH)
 		time.sleep(.1)
@@ -89,7 +72,7 @@ elif (led.lower() == "blue"):
 		GPIO.output(27,GPIO.LOW)
 		time.sleep(1)
 else:
-	print "Something went terribly wrong!"
+	print "Something went terribly wrong!" #for incorrect input 
 		
 
 
