8140de79e3144f0462fc1c085515eeee54a89f7f
Sofia Chatzopoulou <sofaki12@msn.com>
2017-05-11 18:24:19 +0300

    Update les2.py

--- les2.py
+++ les2.py
@@ -7,20 +7,25 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 #GPIO.cleanup()
 GPIO.setwarnings(False)	
+#red light declaration
 GPIO.setup(17,GPIO.OUT)
+#blue light declaration
 GPIO.setup(27,GPIO.OUT)
+#button declaration 
 GPIO.setup(10,GPIO.IN)
 time.sleep(2)
+#wait 2seconds, if button is pressed then turn on leds
 if ~(GPIO.input(10)):
 	print "Lights on"
 	GPIO.output(17,GPIO.HIGH)
 	GPIO.output(27,GPIO.HIGH)
-else:
+else:  #else turn off the leds
 	print "Lights off"
 	GPIO.output(17,GPIO.LOW)
 	GPIO.output(27,GPIO.LOW)
 
-"""for i in range(1,11):
+#blink both leds for ten times	
+for i in range(1,11):
 	print "Lights on"
 	GPIO.output(17,GPIO.HIGH)
 	GPIO.output(27,GPIO.HIGH)
@@ -29,4 +34,3 @@ else:
 	GPIO.output(17,GPIO.LOW)
 	GPIO.output(27,GPIO.LOW)
 	time.sleep(1)
-"""
